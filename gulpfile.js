d10d63b3c05d9c1e41bb0f21598961250cf7800d
Pavel <pahonski@gmail.com>
2018-08-31 14:58:49 +0300

    add server, change bg-s

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const gulp = require('gulp');
+const browsersync = require('browser-sync').create();
+
+gulp.task('serve', function () {
+  browsersync.init({
+    open: true,
+    server: './'
+  });
+  browsersync.watch('./', browsersync.reload);
+});
