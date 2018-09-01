fb53fb4eedbbd0060f65f515c1d386ec2b63644b
Pavel <pahonski@gmail.com>
2018-09-01 18:17:35 +0300

    delete orientation script

--- js/index.js
+++ js/index.js
@@ -18,34 +18,6 @@ const moduleSettings = new ModuleSettings(controller, gameArea);
 
 const moduleRating = new ModuleRating(controller);
 
-var timeout, counter = 0;
-
-var updateCounter = function() {
-  counter++;
-  Array.prototype.forEach.call(document.getElementsByClassName('counter'), function(element) {
-    element.innerHTML = counter;
-  });
-  timeout = setTimeout(updateCounter, 1000);
-};
-
-var orientationQuery = window.matchMedia('(orientation: portrait)');
-
-var orientationHandler = function(portraitOrientation) {
-  if (portraitOrientation.matches) {
-    document.body.className = 'portrait';
-    updateCounter();
-  } else {
-    document.body.className = 'landscape';
-    clearTimeout(timeout);
-  }
-};
-
-// get notified when orientation changes
-orientationQuery.addListener(orientationHandler);
-
-// detect current orientation
-orientationHandler(orientationQuery);
-
 
 
 
